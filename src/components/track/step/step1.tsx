032c72fb8e4cdec12cc5d02af49c475a4d358136
Bui Minh Tien <minhtien100396@gmail.com>
2026-01-12 21:55:25 +0700

    1

--- src/components/track/step/step1.tsx
+++ src/components/track/step/step1.tsx
@@ -1,14 +1,48 @@
-'use client'
-
-import { dark } from "@mui/material/styles/createPalette"
-import { FileWithPath, useDropzone } from 'react-dropzone';
-import './theme.css'
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+"use client";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import { useCallback } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+import "./theme.css";
 
 const Step1 = () => {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        // Do something with the files
+        console.log(">>> check ondrop: ");
+    }, []);
+
+    const VisuallyHiddenInput = styled("input")({
+        clip: "rect(0 0 0 0)",
+        clipPath: "inset(50%)",
+        height: 1,
+        overflow: "hidden",
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        whiteSpace: "nowrap",
+        width: 1,
+    });
+
+    function InputFileUpload() {
+        return (
+            <>
+                <Button
+                    onClick={(e) => e.preventDefault()}
+                    component="label"
+                    variant="contained"
+                    startIcon={<CloudUploadIcon />}
+                >
+                    Upload file
+                    <VisuallyHiddenInput type="file" {...getInputProps()} />
+                </Button>
+            </>
+        );
+    }
+
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+        onDrop,
+    });
 
     const files = acceptedFiles.map((file: FileWithPath) => (
         <li key={file.path}>
@@ -16,53 +50,21 @@ const Step1 = () => {
         </li>
     ));
 
-    const VisuallyHiddenInput = styled('input')({
-        clip: 'rect(0 0 0 0)',
-        clipPath: 'inset(50%)',
-        height: 1,
-        overflow: 'hidden',
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        whiteSpace: 'nowrap',
-        width: 1,
-    });
-
-
     return (
-        <section className="container">
-            <div {...getRootProps({ className: 'dropzone' })}>
-                <input {...getInputProps()} />
-                <InputFileUpload />
-                <p>Click hoặc Drag/Drop để upload file track</p>
-            </div>
-            <aside>
-                <h4>Files</h4>
-                <ul>{files}</ul>
-            </aside>
-        </section>
+        <>
+            <section className="container">
+                <div {...getRootProps({ className: "dropzone" })}>
+                    {/* <input {...getInputProps()} /> */}
+                    <InputFileUpload />
+                    <p>Click hoặc Drag/Drop để upload file track</p>
+                </div>
+                <aside>
+                    <h4>Files</h4>
+                    <ul>{files}</ul>
+                </aside>
+            </section>
+        </>
     );
-
-
-    function InputFileUpload() {
-        return (
-            <Button
-                onClick={(e) => {
-                    e.stopPropagation()
-                }}
-                component="label"
-                role={undefined}
-                variant="contained"
-                tabIndex={-1}
-                startIcon={<CloudUploadIcon />}
-            >
-                Upload files
-                <VisuallyHiddenInput
-                    type="file"
-                />
-            </Button>
-        );
-    }
-}
+};
 
 export default Step1;
