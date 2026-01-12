627f6b9f56ba83dde73144896461e357ed55ebfd
Bui Minh Tien <minhtien100396@gmail.com>
2026-01-12 07:34:40 +0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
-import AppFooter from '@/components/footer/app.footer';
-import AppHeader from '@/components/header/app.header';
-import ThemeRegistry from '@/components/theme-registry/theme.registry';
+import AppFooter from "@/components/footer/app.footer";
+import AppHeader from "@/components/header/app.header";
+import ThemeRegistry from "@/components/theme-registry/theme.registry";
+import NextAuthWrapper from "./lib/next.auth.wrapper";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <ThemeRegistry>
-          <AppHeader />
-          {children}
-          <AppFooter />
-        </ThemeRegistry>
-      </body>
-    </html >
-  );
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <ThemeRegistry>
+                    <NextAuthWrapper>
+                        <AppHeader />
+                        {children}
+                        <AppFooter />
+                    </NextAuthWrapper>
+                </ThemeRegistry>
+            </body>
+        </html>
+    );
 }
--- src/components/header/app.header.tsx
+++ src/components/header/app.header.tsx
@@ -1,71 +1,70 @@
-'use client'
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+"use client";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Badge from "@mui/material/Badge";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { alpha, styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import * as React from "react";
 
-
-
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
         backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
         marginLeft: theme.spacing(3),
-        width: 'auto',
+        width: "auto",
     },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
+    color: "inherit",
+    "& .MuiInputBase-input": {
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '400px',
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: "400px",
         },
     },
 }));
 
 export default function AppHeader() {
-    const router = useRouter()
+    const { data: session } = useSession();
+
+    const router = useRouter();
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
         React.useState<null | HTMLElement>(null);
@@ -90,65 +89,74 @@ export default function AppHeader() {
         setMobileMoreAnchorEl(event.currentTarget);
     };
 
-    const menuId = 'primary-search-account-menu';
+    const menuId = "primary-search-account-menu";
     const renderMenu = (
         <Menu
             anchorEl={anchorEl}
             anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
+                vertical: "bottom",
+                horizontal: "right",
             }}
             id={menuId}
             keepMounted
             transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             open={isMenuOpen}
-            onClose={handleMenuClose}
+            onClose={() => {
+                handleMenuClose();
+                signOut();
+            }}
         >
             <MenuItem>
-                <Link href={"/profile"}
+                <Link
+                    href={"/profile"}
                     style={{
                         color: "unset",
-                        textDecoration: "unset"
+                        textDecoration: "unset",
                     }}
                 >
                     Profile
                 </Link>
             </MenuItem>
             <MenuItem>
-                <Link href={"logout"}
+                <Link
+                    href={"logout"}
                     style={{
                         color: "unset",
-                        textDecoration: "unset"
+                        textDecoration: "unset",
                     }}
                 >
                     Logout
                 </Link>
             </MenuItem>
-        </Menu >
+        </Menu>
     );
 
-    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const mobileMenuId = "primary-search-account-menu-mobile";
     const renderMobileMenu = (
         <Menu
             anchorEl={mobileMoreAnchorEl}
             anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             id={mobileMenuId}
             keepMounted
             transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             open={isMobileMenuOpen}
             onClose={handleMobileMenuClose}
         >
             <MenuItem>
-                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                <IconButton
+                    size="large"
+                    aria-label="show 4 new mails"
+                    color="inherit"
+                >
                     <Badge badgeContent={4} color="error">
                         <MailIcon />
                     </Badge>
@@ -183,14 +191,15 @@ export default function AppHeader() {
     );
 
     const handleRedirectHome = () => {
-        router.push("/")
-    }
+        router.push("/");
+    };
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static"
+            <AppBar
+                position="static"
                 sx={{
-                    background: "#4c5c6c"
+                    background: "#4c5c6c",
                 }}
             >
                 <Container>
@@ -200,9 +209,9 @@ export default function AppHeader() {
                             noWrap
                             component="div"
                             sx={{
-                                display: { xs: 'none', sm: 'block' },
+                                display: { xs: "none", sm: "block" },
 
-                                cursor: "pointer"
+                                cursor: "pointer",
                             }}
                             onClick={() => handleRedirectHome()}
                         >
@@ -214,26 +223,40 @@ export default function AppHeader() {
                             </SearchIconWrapper>
                             <StyledInputBase
                                 placeholder="Search…"
-                                inputProps={{ 'aria-label': 'search' }}
+                                inputProps={{ "aria-label": "search" }}
                             />
                         </Search>
                         <Box sx={{ flexGrow: 1 }} />
-                        <Box sx={{
-                            display: { xs: 'none', md: 'flex' },
-                            gap: "20px",
-                            alignItems: "center",
-                            cursor: "pointer",
-                            "> a": {
-                                color: "unset",
-                                textDecoration: "unset"
-                            }
-                        }}>
-                            <Link href={"/playlist"}>Playlists</Link>
-                            <Link href={"/like"}>Likes</Link>
-                            <span>Upload</span>
-                            <Avatar onClick={handleProfileMenuOpen}>ER</Avatar>
+                        <Box
+                            sx={{
+                                display: { xs: "none", md: "flex" },
+                                gap: "20px",
+                                alignItems: "center",
+                                cursor: "pointer",
+                                "> a": {
+                                    color: "unset",
+                                    textDecoration: "unset",
+                                },
+                            }}
+                        >
+                            {session ? (
+                                <>
+                                    <Link href={"/playlist"}>Playlists</Link>
+                                    <Link href={"/like"}>Likes</Link>
+                                    <span>Upload</span>
+                                    <Avatar onClick={handleProfileMenuOpen}>
+                                        ER
+                                    </Avatar>
+                                </>
+                            ) : (
+                                <>
+                                    <Link href={"#"} onClick={() => signIn()}>
+                                        Login
+                                    </Link>
+                                </>
+                            )}
                         </Box>
-                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <Box sx={{ display: { xs: "flex", md: "none" } }}>
                             <IconButton
                                 size="large"
                                 aria-label="show more"
