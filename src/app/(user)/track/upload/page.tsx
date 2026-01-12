962f916b84adfb5d6a9541fec873f431c6298461
BRYCENVN\bm_tien <bm_tien@brycen.com.vn>
2026-01-12 19:27:38 +0900

    update upload

---
+++ src/app/(user)/track/upload/page.tsx
@@ -0,0 +1,17 @@
+
+import UploadTabs from "@/components/track/upload.tabs";
+import { Container } from "@mui/material";
+
+
+
+const UploadPage = () => {
+    return (
+        <div>
+            <Container>
+                <UploadTabs />
+            </Container>
+        </div>
+    )
+}
+
+export default UploadPage;
